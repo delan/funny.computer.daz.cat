e866fa7ec9b111a88c1484dd4069ecf595a59f3d
Delan Azabani <delan@azabani.com>
2023-07-18 21:24:49 +0800

    add edit warning because i keep editing the wrong file by mistake

--- build.sh
+++ build.sh
@@ -3,9 +3,16 @@ set -eu
 . ./pages.inc.sh
 
 for i; do
-    title=$(rg -o --pcre2 '(?<=<!-- \[title = ).+(?=] -->)' -- "$i.in.html" || :)
+    # works with openbsd ripgrep, which has no --pcre2
+    title=$(< "$i.in.html" rg -o '<!-- \[title = .+\] -->' | sed -E 's/<!-- \[title = |\] -->$//g')
+
+    # only works with bsd stat(1), not gnu stat(1)
     mtime=$(TZ= stat -f '%Sm' -t '%FT%RZ' -- "$i.in.html")
-    sed 's/<!-- \[title\] -->/'"$title"'/' header.inc.html > "$i.out.html"
+
+    sed '
+        s/<!-- \[edit warning\] -->/<!-- WARNING: you probably want to edit the .in.html, not this file! -->/
+        s/<!-- \[title\] -->/'"$title"'/
+    ' header.inc.html > "$i.out.html"
     cat "$i.in.html" >> "$i.out.html"
     sed 's/<!-- \[mtime\] -->/'"$mtime"'/' footer.inc.html >> "$i.out.html"
     colordiff -u -- "$i.html" "$i.out.html" || :
