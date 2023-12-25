60895f2c105983f1befd68aea80cda78d9028212
Delan Azabani <delan@azabani.com>
2023-12-25 16:57:24 +0800

    run bsd date(1) with -u as well

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ if [ "$(stat -c \%n /dev/null 2> /dev/null)" = /dev/null ] \
         date -ud \@$(stat -c \%Y -- "$1") +\%FT\%RZ
     }
 elif [ "$(stat -f \%N /dev/null 2> /dev/null)" = /dev/null ] \
-&& [ "$(date -jr 0 +\%FT\%RZ 2> /dev/null)" = 1970-01-01T00:00Z ]; then
+&& [ "$(date -ujr 0 +\%FT\%RZ 2> /dev/null)" = 1970-01-01T00:00Z ]; then
     # bsd stat(1) + date(1)
     mtime() {
-        date -jr $(stat -f \%m -- "$1") +\%FT\%RZ
+        date -ujr $(stat -f \%m -- "$1") +\%FT\%RZ
     }
 else
     >&2 echo 'fatal: no compatible stat(1) + date(1)'
