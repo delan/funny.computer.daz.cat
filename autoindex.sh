e22726a5d17deb81d4039166fb2b0413f1c41cd5
Delan Azabani <delan@azabani.com>
2023-12-25 21:02:18 +0800

    fix bugs in autoindex scripts after soupault migration

--- autoindex.sh
+++ autoindex.sh
@@ -7,7 +7,7 @@ for i; do
     > "site/$i/index.in" printf '<h1 class=title>/%s/</h1>\n' "$i"
     >> "site/$i/index.in" printf '<table rules=cols cellpadding=4>\n'
     >> "site/$i/index.in" printf '<tr><td>d<td>-<td><a href="..">.. (parent directory)</a>\n'
-    ls -l -- "$i" | sed -Ef autoindex.sed >> "site/$i/index.in"
+    ls -l -- "site/$i" | sed -Ef autoindex.sed >> "site/$i/index.in"
     >> "site/$i/index.in" printf '</table>\n'
 done
 
